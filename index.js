952da1694aadab153b477c8d22322a4d049e959a
DanielHatz/ <110106014+DanielHatz@users.noreply.github.com>
2025-01-14 15:42:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Definir a versão da API
 let apiVersion = '2.0.3';  // A versão pode ser atualizada no CI/CD
